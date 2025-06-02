bae7cc5c2c555d5e672c846a7540992a210387a5
slilonfe5 <slilonfe@nd.edu>
2025-06-02 17:13:18 -0400

    Added new files

--- shedlovsky_conductivity_pyomo.py
+++ shedlovsky_conductivity_pyomo.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import math
 import scipy.stats as stats
 import pandas as pd
+import pyomo.environ as pyo
 
 # Shedlovsky model for electrolyte equivalent conductivity
 def _shedlovsky(conc, temp, epsilon, eta, lambda_0, a, z_1, z_2, lambda_0_cation, lambda_0_anion):
@@ -11,7 +12,7 @@ def _shedlovsky(conc, temp, epsilon, eta, lambda_0, a, z_1, z_2, lambda_0_cation
     Calculates equivalent conductivity using equation (7.36) in reference
 
     Arguments:
-        conc: list of the salt concentration in M
+        conc: integer or float value of the salt concentration in M
         temp: integer or float value of the temperature in K
         epsilon:integer or float value of the dielectric constant
         eta: integer or float value of the viscosity of the salt solution in poise
@@ -28,30 +29,26 @@ def _shedlovsky(conc, temp, epsilon, eta, lambda_0, a, z_1, z_2, lambda_0_cation
     Returns:
         equiv_cond: equivalent conductivity of the salt in cm^2.S/equiv
     """
-    # calculate the B and q constants
+    # calculating B and q
     B = 50.29 * (10 ** 8) * (epsilon * temp) ** (-0.5)
     q = np.abs(z_1 * z_2) * (lambda_0_cation + lambda_0_anion) / (
                 (np.abs(z_1) + np.abs(z_2)) * (np.abs(z_2) * lambda_0_cation + np.abs(z_1) * lambda_0_anion))
 
-    # calculate the relaxation correction, B1, and the hydrodynamic correction, B2
-    B_1 = 2.801 * 10 ** 6 * np.abs(z_1 * z_2) * q / ((epsilon * temp) ** (3 / 2) * (1 + math.sqrt(q)))
+    # B1 and B2
+    B_1 = 2.801 * 10 ** 6 * np.abs(z_1 * z_2) * q / ((epsilon * temp) ** (3 / 2) * (1 + pyo.sqrt(q)))
     B_2 = 41.25 * (np.abs(z_1) + np.abs(z_2)) / (eta * (epsilon * temp) ** (1 / 2))
 
     # ion concentration
     cation_conc = conc
-    Cl_conc = [np.abs(z_1) * conc[i] for i in range(len(conc))]
+    Cl_conc = np.abs(z_1) * conc
 
     # calculate the ionic strength
-    I = np.zeros(len(conc))
-    for i in range(len(conc)):
-        conc_all = [cation_conc[i], Cl_conc[i]]
-        z = [z_1, z_2]
-        I[i] = 1 / 2 * (sum(conc_all[j] * z[j] ** 2 for j in range(len(conc_all))))
+    conc_all = [cation_conc, Cl_conc]
+    z = [z_1, z_2]
+    I = 1 / 2 * (sum(conc_all[j] * z[j] ** 2 for j in range(len(conc_all))))
 
     # calculate the salt equivalent conductivity
-    equiv_cond = []
-    for i in range(len(conc)):
-        equiv_cond.append(lambda_0 - ((B_1 * lambda_0 + B_2) * math.sqrt(I[i]) / (1 + a * B * math.sqrt(I[i]))))
+    equiv_cond = lambda_0 - ((B_1 * lambda_0 + B_2) * pyo.sqrt(I) / (1 + a * B * pyo.sqrt(I)))
 
     return equiv_cond
 
@@ -62,7 +59,7 @@ def variant_shedlovsky(conc, temp, epsilon, eta, lambda_0, a, z_1, z_2, lambda_0
     Calculates specific conductivity from equivalent conductivity
 
     Arguments:
-        conc: list of the salt concentration in M
+        conc: integer or float value of the salt concentration in M
         temp: integer or float value of the temperature in K
         epsilon:integer or float value of the dielectric constant
         eta: integer or float value of the viscosity of the salt solution in poise
@@ -87,14 +84,12 @@ def variant_shedlovsky(conc, temp, epsilon, eta, lambda_0, a, z_1, z_2, lambda_0
     cation_conc = conc
 
     # convert the cation concentrations from M to equivalent per litre
-    equiv_conc = [cation_conc[i] * np.abs(z_1) for i in range(len(conc))]
+    equiv_conc = cation_conc * np.abs(z_1)
 
     # calculate the salt specific conductivity in Siemen per cm
-    specific_cond = []
-    for i in range(len(conc)):
-        specific_cond.append((equiv_conc[i] / 1000) * equiv_cond[i])
+    specific_cond = (equiv_conc / 1000) * equiv_cond
 
     # specific conductivity in micro Siemen per cm
-    specific_cond_calc = [specific_cond[i] * 10 ** 6 for i in range(len(conc))]
+    specific_cond_calc = specific_cond * 10 ** 6
 
     return specific_cond_calc
